74f27cc96a04ad8a023433b3332ce0c55858fb66
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-02-06 15:45:44 -0500

    homework completed

---
+++ DictionariesPractice/Membership.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionariesPractice
+{
+    public class Membership
+    {
+        //fields
+        private int memberID;
+        private bool isActive;
+
+        //properties
+        public int MemberID
+        {
+            get { return this.memberID; }
+        }
+
+        public bool IsActive
+        {
+            get { return this.isActive; }
+        }
+
+        //constructors
+
+        //methods
+    }
+}
--- DictionariesPractice/Program.cs
+++ DictionariesPractice/Program.cs
@@ -43,10 +43,10 @@ namespace DictionariesPractice
             Console.WriteLine(drinkMachine.Count);
 
             //.Key property:
-            foreach(KeyValuePair<int, string> drink in drinkMachine)
-                {
+            foreach (KeyValuePair<int, string> drink in drinkMachine)
+            {
                 Console.WriteLine(drink.Key);
-                }
+            }
             foreach (KeyValuePair<int, string> drink in drinkMachine)
             {
                 Console.WriteLine(drink.Value);
@@ -66,7 +66,7 @@ namespace DictionariesPractice
                 {09, "ski jacket" },
                 {10, "carhart" }
             };
-            foreach(KeyValuePair<int, string>coat in coatCheck)
+            foreach (KeyValuePair<int, string> coat in coatCheck)
             {
                 Console.WriteLine(coat.Value);
             }
@@ -89,7 +89,7 @@ namespace DictionariesPractice
             //zoo animals dictionary
             Dictionary<string, int> zooAnimals = new Dictionary<string, int>()
             {
-                {"lion", 2 },
+                {"lion", 4 },
                 {"penguin", 20 },
                 {"sloth", 7 },
                 {"zebra", 12 },
@@ -101,6 +101,10 @@ namespace DictionariesPractice
                 {"snow leopard", 2 },
             };
 
+            Console.WriteLine("There are currently {0} types of animals at the zoo.", zooAnimals.Count);
+
+
+
             //to print the highest quantity:
             int highest = 0;
 
@@ -111,16 +115,66 @@ namespace DictionariesPractice
                     highest = animal.Value;
                 }
             }
-            foreach(KeyValuePair<string, int> animal in zooAnimals)
+            foreach (KeyValuePair<string, int> animal in zooAnimals)
             {
-                if(animal.Value == highest)
+                if (animal.Value == highest)
                     Console.WriteLine("The animal with the highest population at the zoo: " + animal.Key);
             }
 
             //remove lowest quantity animal:
+            int lowest = highest;
+            foreach (KeyValuePair<string, int> animal in zooAnimals)
+            {
+                if (animal.Value < lowest)
+                {
+                    lowest = animal.Value;
+                }
+            }
+            string lowestAnimalPop = "0";
+            foreach (KeyValuePair<string, int> animal in zooAnimals)
+            {
+                if (animal.Value == lowest)
+                {
+                    Console.WriteLine("the animal with the lowest population is the " + animal.Key);
+                    lowestAnimalPop = animal.Key;
+                    
+                }
+            }
+            zooAnimals.Remove(lowestAnimalPop);
+
             //count remaining and print to console
+            Console.WriteLine("You moved an animal to another zoo, now there are {0} types of animals.", zooAnimals.Count);
+
+
             //allow user to input animal name and check to see if dictionary contains that animal
             //ask user if they'd like to add any animal that doesn't exist in dictionary
+
+            Console.WriteLine("Please type the name of the animal you'd like to check: ");
+            string userAnimal = Console.ReadLine();
+            if (zooAnimals.ContainsKey(userAnimal) == true)
+            {
+                Console.WriteLine("{0} is in the dictionary.", userAnimal);
+            }
+            else
+            {
+                Console.WriteLine("This animal is not in the dictionary.  Would you like to add it?");
+                string userAnswer = Console.ReadLine().ToLower();
+                if (userAnswer == "yes")
+                {
+                    Console.WriteLine("How many animals of that type are there?");
+                    int populationUserAnswer = int.Parse(Console.ReadLine());
+                    zooAnimals.Add(userAnimal, populationUserAnswer);
+                }
+                else if (userAnswer == "no")
+                {
+                    Console.WriteLine("Ok, we won't add it.");
+                }
+                else
+                {
+                    Console.WriteLine("invalid response!");
+                }
+            }
+
         }
     }
 }
